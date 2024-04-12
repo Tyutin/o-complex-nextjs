6c8d51363942a95d404215292bb99f98daf80cec
Tyutin Kirill <kerya.tyutin@yandex.ru>
2024-04-12 22:33:01 +0400

    header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '../assets/global.scss';
+import Header from '@/components/Header/Header';
 
 const inter = Inter({ subsets: ['latin', 'cyrillic'] });
 
@@ -18,7 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import Card from '../UI/Card/Card';
+import './Header.scss';
+
+export default function Header() {
+  return (
+    <header className="header">
+      <Card className="container header__content">тестовое задание</Card>
+    </header>
+  );
+}
