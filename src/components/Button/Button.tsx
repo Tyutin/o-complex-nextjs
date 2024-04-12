a51b803b5810fa678c96a5c41d5ec3802e3a5d0a
Tyutin Kirill <kerya.tyutin@yandex.ru>
2024-04-12 16:22:26 +0400

    Button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,28 @@
+import { ReactNode } from 'react';
+import './Button.scss';
+import classNames from 'classnames';
+
+type ButtonProps = {
+  children: ReactNode;
+  onClick: () => void;
+  className?: string;
+  disabled?: boolean;
+  type?: 'button' | 'reset' | 'submit';
+};
+
+export default function Button({
+  children,
+  className,
+  onClick,
+  disabled = false,
+  type = 'submit',
+}: ButtonProps) {
+  return (
+    <button
+      className={classNames('button', className)}
+      {...{ onClick, disabled, type }}
+    >
+      {children}
+    </button>
+  );
+}
